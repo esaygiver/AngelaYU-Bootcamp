42c141bedd9202b1c6cc1a1f6e1bd747c4cbf002
Emirhan Saygiver <73871735+esaygiver@users.noreply.github.com>
2021-01-27 22:43:15 +0300

    fourth commit

---
+++ InspoQuotes/InspoQuotes/quotesToShow.swift
@@ -0,0 +1,14 @@
+//
+//  quotesToShow.swift
+//  InspoQuotes
+//
+//  Created by admin on 16.11.2020.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuotesToShow {
+    var body: String = ""
+}
